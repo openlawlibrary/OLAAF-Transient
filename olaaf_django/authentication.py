8ea7179100258ce4a78f9932d26d0d8d8919fba8
Daniel Elero <danixeee@gmail.com>
2020-04-22 19:47:22 +0200

    Update view WIP

--- olaaf_django/authentication.py
+++ olaaf_django/authentication.py
@@ -1,16 +1,14 @@
-import mimetypes
 import datetime
+import mimetypes
+
 from lxml import etree as et
 
 from .models import Hash, Publication
 from .utils import (calc_hash, get_auth_div_content, get_html_document,
-                    strip_binary_content, reset_local_urls)
-
-# TODO we need to store the information about which publication the document
-# belongs to (and to which repository that publication belongs to)
+                    reset_local_urls, strip_binary_content)
 
 
-def check_authenticity(date, url, content):
+def check_authenticity(publication, date, url, content):
   if not url.startswith('/'):
     url = '/' + url
   content_type, _ = mimetypes.guess_type(url)
@@ -21,13 +19,12 @@ def check_authenticity(date, url, content):
   }.get(file_type)
 
   if hashing_func is None:
-    return AuthetnicationResponse(url, authenticable=False)
+    return AuthenticationResponse(url, authenticable=False)
 
   if file_type == 'html' and date is not None:
     content = reset_local_urls(content, date)
 
   hash_value = hashing_func(content)
-  publication = Publication.objects.order_by('-name')[0]
 
   if date is not None:
     date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
@@ -35,26 +32,26 @@ def check_authenticity(date, url, content):
   hash_type = Hash.RENDERED if file_type == 'html' else Hash.BITSTREAM
   hash_data = Hash.objects.filter(path__url=url, value=hash_value, hash_type=hash_type,
                                   start_commit__publication=publication). \
-                                  values('start_commit__date', 'end_commit__date')
+      values('start_commit__date', 'end_commit__date')
 
   if not len(hash_data):
     # not authentic
-    return AuthetnicationResponse(url, date=date)
+    return AuthenticationResponse(url, date=date)
 
   start_commit_date = hash_data[0]['start_commit__date']
   end_commit_date = hash_data[0]['end_commit__date']
 
   if date is None:
     if end_commit_date is None:
-      return AuthetnicationResponse(url, authentic=True, current=True, from_date=start_commit_date)
+      return AuthenticationResponse(url, authentic=True, current=True, from_date=start_commit_date)
     else:
-      return AuthetnicationResponse(url, authentic=True, from_date=start_commit_date,
+      return AuthenticationResponse(url, authentic=True, from_date=start_commit_date,
                                     to_date=end_commit_date)
 
   if date >= start_commit_date and (end_commit_date is None or date <= end_commit_date):
-    return AuthetnicationResponse(url, authentic=True, from_date=start_commit_date, date=date)
+    return AuthenticationResponse(url, authentic=True, from_date=start_commit_date, date=date)
   else:
-    return AuthetnicationResponse(url, authentic=False, date=date)
+    return AuthenticationResponse(url, authentic=False, date=date)
 
 
 def _calculate_binary_content_hash(binary_content):
@@ -68,7 +65,7 @@ def _calculate_html_hash(html_content):
   return calc_hash(et.tostring(body_section))
 
 
-class AuthetnicationResponse():
+class AuthenticationResponse():
 
   def __init__(self, url, authenticable=True, authentic=False, current=False, from_date=None,
                to_date=None, date=None):
