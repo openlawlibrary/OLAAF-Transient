9e3795dd33869b5e4a6f801d1f0f0a6b6429a75f
Daniel Elero <danixeee@gmail.com>
2020-04-27 10:48:37 +0200

    Change hardcoded endpoints

--- extensions/edge-extension/authenticate.js
+++ extensions/edge-extension/authenticate.js
@@ -1,42 +1,40 @@
-content = new XMLSerializer().serializeToString(document)
-href = window.location.href
-url = new URL(href).pathname
+content = new XMLSerializer().serializeToString(document);
+href = window.location.href;
+url = new URL(href).pathname;
 
 var xhr = new XMLHttpRequest();
-xhr.open("POST", 'http://localhost:8000/_api/authenticate', true);
+xhr.open("POST", `${window.location.origin}/_api/authenticate`, true);
 
-if (href.endsWith('.pdf')){
+if (href.endsWith(".pdf")) {
   JSAlert.alert("Validation initiated...").dismissIn(1000 * 3);
   var request = new XMLHttpRequest();
-  request.open('GET', href, true);
-  request.responseType = 'blob'
+  request.open("GET", href, true);
+  request.responseType = "blob";
   request.send(null);
   request.onreadystatechange = function () {
     if (request.readyState === 4 && request.status === 200) {
-      blob = request.response
-      if (blob != null){
-        xhr.onreadystatechange = function() {
+      blob = request.response;
+      if (blob != null) {
+        xhr.onreadystatechange = function () {
           if (xhr.readyState == 4) {
-            JSAlert.alert(`${href}<br/><br/>${xhr.responseText}`)
-            }
+            JSAlert.alert(`${href}<br/><br/>${xhr.responseText}`);
           }
-          var formData = new FormData();
-          formData.append('content', blob);
-          formData.append('url', url)
-          xhr.send(formData);
+        };
+        var formData = new FormData();
+        formData.append("content", blob);
+        formData.append("url", url);
+        xhr.send(formData);
       }
     }
-  }
-}
-else{
-
-  content = encodeURIComponent(content)
-  params = `url=${url}&content=${content}`
+  };
+} else {
+  content = encodeURIComponent(content);
+  params = `url=${url}&content=${content}`;
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr.onreadystatechange = function() {
+  xhr.onreadystatechange = function () {
     if (xhr.readyState == 4) {
-      JSAlert.alert(`${href}<br/><br/>${xhr.responseText}`)
+      JSAlert.alert(`${href}<br/><br/>${xhr.responseText}`);
     }
-  }
+  };
   xhr.send(params);
 }
