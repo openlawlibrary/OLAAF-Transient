282ce47ea3f0264fcd1221567320072205291d69
Renata <vrenata8@gmail.com>
2020-06-15 14:29:48 +0200

    Formatting

--- olaaf_django/sync_hashes.py
+++ olaaf_django/sync_hashes.py
@@ -72,8 +72,8 @@ def sync_hashes(library_root, repos_data):
           date = commits_data[0]["additional-info"]["build-date"]
           try:
             publication = Publication.objects.create(repository=repository,
-                                                    name=publication_name,
-                                                    date=date)
+                                                     name=publication_name,
+                                                     date=date)
           except Exception as e:
             logger.error('Could not create publication %s due to error:\n%s',
                          publication_name, str(e))
