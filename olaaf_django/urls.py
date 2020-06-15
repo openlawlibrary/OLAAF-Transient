93a832d437211f959a95e76b4976619028e554aa
Daniel Elero <danixeee@gmail.com>
2020-06-15 19:17:59 +0200

    Serve index page

--- olaaf_django/urls.py
+++ olaaf_django/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-  path('authenticate', views.authenticate, name='authenticate'),
+    path('authenticate', views.authenticate, name='authenticate'),
 ]
