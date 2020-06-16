281bc85af5e3082f16c5aeedf9a7a03b1c988a9e
Daniel Elero <danixeee@gmail.com>
2020-06-16 16:16:13 +0200

    Check files authennticity API

--- olaaf_django/urls.py
+++ olaaf_django/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('authenticate', views.authenticate, name='authenticate'),
+    path('check-hashes', views.check_hashes, name='check-hashes'),
 ]
--- olaaf_django/views.py
+++ olaaf_django/views.py
@@ -1,12 +1,13 @@
+import json
 import re
 
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
 from . import get_repo_info
 from .authentication import AuthenticationResponse, check_authenticity
-from .models import Publication
+from .models import Hash, Publication
 
 URL_RE = re.compile(
     r'((\/)?_publication\/(?P<pub>(\d{4}-\d{2}(-\d{2})?(-\d{2})?)))?' +  # backwards compatible
@@ -38,6 +39,36 @@ def authenticate(request):
   auth_response = check_authenticity(publication, pub_name, date, path, url, content, content_type)
 
   return auth_response.to_http_response(request)
+
+
+@csrf_exempt
+@require_http_methods(['POST'])
+def check_hashes(request):
+  results = {}
+  try:
+    data = json.loads(request.body)
+    for file_info in data:
+      file_name = file_info.get('fileName')
+      file_hash = file_info.get('fileHash')
+
+      try:
+        hash_obj = Hash.objects.get(value=file_hash)
+
+        authentic = True
+        current = hash_obj.end_commit_id is None
+      except Exception:
+        authentic = False
+        current = False
+
+      results[file_name] = dict(
+          fileName=file_name,
+          authentic=authentic,
+          current=current,
+      )
+  except Exception:
+    pass
+
+  return JsonResponse(results)
 
 
 def _extract_url(url):
