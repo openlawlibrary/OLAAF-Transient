f4930bb212bd703aba11ce850d0f5d6d81a23cf8
Renata <rvaderna@openlawlib.org>
2022-06-01 02:54:46 +0200

    Release 0.19.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 PACKAGE_NAME = 'olaaf-transient'
-VERSION = '0.18.0'
+VERSION = '0.19.0'
 AUTHOR = 'Open Law Library'
 AUTHOR_EMAIL = 'info@openlawlib.org'
 DESCRIPTION = 'Implementation of transient authentication'
